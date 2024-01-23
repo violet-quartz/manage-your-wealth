6ee3c59bef21a8e1d4220a7800aa6e83cc197083
Flora Ma <mczggy@gmail.com>
2024-01-23 22:23:16 +0800

    Add more functions.

--- application/account.py
+++ application/account.py
@@ -17,50 +17,44 @@ def index():
     if g.user is None:
         return render_template('account/index.html')
     db = get_db()
-    # list account balance
-    balance = db.execute(
-        'SELECT * FROM balance WHERE user_id = ?',
-        (g.user['id'],)
+    today = datetime.now().date()
+    # list projects
+    ongoing_projects = db.execute(
+        'SELECT p.id, p.name, p.details, IFNULL(total_income, 0) - IFNULL(total_expense, 0) AS balance, '
+        'IFNULL(IFNULL(i.unit, e.unit), "") AS unit FROM '
+        '(SELECT id, name, details FROM project WHERE user_id = ? AND end_date >= ?) AS p '
+        'LEFT OUTER JOIN '
+        '(SELECT project_id, SUM(amount) AS total_expense, unit FROM expense GROUP BY project_id, unit) AS e '
+        'ON e.project_id = p.id '
+        'LEFT OUTER JOIN '
+        '(SELECT project_id, SUM(amount) AS total_income, unit FROM income GROUP BY project_id, unit) AS i '
+        'ON i.project_id = p.id '
+        'WHERE e.unit = i.unit OR e.unit IS NULL OR i.unit IS NULL',        
+        (g.user['id'], today)
         ).fetchall()
-    # list recent 3 earnings
-    earnings = db.execute(
-        'SELECT * FROM income WHERE user_id = ? ORDER BY date DESC LIMIT ?',
-        (g.user['id'], 3)
-        ).fetchall()
-    # list recent 5 expenses
-    expenses = db.execute(
-        'SELECT * FROM expense WHERE user_id = ? ORDER BY date DESC LIMIT ?',
-        (g.user['id'], 5)
-        ).fetchall()
+    
     # list savings 
     saving = db.execute(
-        'SELECT SUM(amount) AS amount, unit FROM saving_account WHERE user_id = ?',
+        'SELECT IFNULL(SUM(amount), 0) AS amount, IFNULL(unit, "") AS unit FROM saving_account WHERE user_id = ?',
         (g.user['id'],)
         ).fetchall()
     
-    today = datetime.now().date()
-    # list ongoing projects    
-    projects = db.execute(
-        'SELECT * FROM project WHERE user_id = ? AND end_date >= ?',
-        (g.user['id'], today)
-        ).fetchall()
 
     # list ongoing investments
     investments = db.execute(
-        'SELECT * FROM investment WHERE user_id = ? AND status = "ongoing" ',
+        'SELECT id, name, type, invest_date, investment.amount, investment.unit, details, '
+        'IFNULL(it.amount, "") AS latest_amount, '
+        'IFNULL(it.unit, "") AS latest_unit FROM investment LEFT OUTER JOIN '
+        '(SELECT investment_id, amount, unit, date FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY investment_id ORDER BY date DESC) AS rn '
+        'FROM investment_track) AS temp WHERE rn = 1) AS it '
+        'ON investment.id = it.investment_id WHERE user_id = ? AND status = "ongoing" ',
         (g.user['id'],)
-        ).fetchall()
-    # list ongoing saving plans
-    saving_plans = db.execute(
-        'SELECT * FROM saving_plan WHERE user_id = ? AND end_date >= ?',
-        (g.user['id'], today)
         ).fetchall()
     
     return render_template('account/index.html', 
-                           balance=balance, earnings=earnings,
-                           expenses=expenses, savings=saving,
-                           projects=projects, investments=investments,
-                           saving_plans=saving_plans)
+                           savings=saving, 
+                           projects=ongoing_projects, 
+                           investments=investments)
 
 @bp.route('/create/expense', methods=['POST', 'GET'])
 @login_required
@@ -158,12 +152,19 @@ def create_investment(source, id):
         details = request.form['details']
 
         db = get_db()
-        db.execute(
+        row = db.execute(
             'INSERT INTO investment(user_id, name, type, details, invest_date, amount, unit, status)'
-            ' VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+            ' VALUES(?, ?, ?, ?, ?, ?, ?, ?) RETURNING id',
             (g.user['id'], name, investment_type, details, invest_date, amount, currency, "ongoing")
-        )
+        ).fetchone()
         db.commit()
+        (inserted_id,) = row
+        db.execute(
+            'INSERT INTO innvestment_track(investment_id, date, amount, unit) '
+            'VALUES(?, ?, ?, ?)',
+            (inserted_id, invest_date, amount, currency)
+        )
+        db.execute()
         return redirect(url_for('account.index'))
 
     return render_template('account/create_investment.html', item=source_item)
@@ -247,6 +248,151 @@ def create_earning():
 
     return render_template('account/create_earning.html', projects=projects)
 
+@bp.route('/create/budget', methods=['GET', 'POST'])
+@login_required
+def create_budget():
+    db = get_db()
+    today = datetime.now().date()
+    projects = db.execute(
+        'SELECT * FROM project WHERE user_id = ? AND end_date >= ?',
+        (g.user['id'], today)
+        ).fetchall()
+    
+    if request.method == 'POST':
+        budget_type = request.form['budget_type']
+        amount = request.form['amount']
+        currency = request.form['currency']
+        details = request.form['details']
+        project_id = request.form['project']
+        res = db.execute(
+            'SELECT * FROM budget WHERE user_id = ? AND project_id = ? AND type = ?',
+            (g.user['id'], project_id, budget_type)
+        ).fetchone()
+        if res:
+            flash(f'Budget {budget_type} has been set. Do you want to update it?')
+            return redirect(url_for('account.update_budget', project_id=project_id, budget_type=budget_type))
+        db.execute(
+            'INSERT INTO budget (user_id, project_id, type, amount, unit, details)'
+            ' VALUES(?, ?, ?, ?, ?, ?)',
+            (g.user['id'], project_id, budget_type, amount, currency, details)
+        )
+        db.commit()
+        return redirect(url_for('account.index'))
+    return render_template('account/create_budget.html', projects=projects)
+
+@bp.route('/update/budget/<project_id>/<budget_type>', methods=['GET', 'POST'])
+@login_required
+def update_budget(project_id, budget_type):
+    db = get_db()
+    budget = db.execute(
+        'SELECT * FROM budget WHERE project_id = ? AND type = ?',
+        (project_id, budget_type)
+        ).fetchone()
+    
+    if request.method == 'POST':
+        amount = request.form['amount']
+        currency = request.form['currency']
+        details = request.form['details']
+        db.execute(
+            'UPDATE budget SET amount = ?, unit = ?, details = ? WHERE user_id = ? AND project_id = ? AND type = ?',
+            (amount, currency, details, g.user['id'], project_id, budget_type)
+        )
+        db.commit()
+        return redirect(url_for('account.index'))
+    return render_template('account/update_budget.html', budget=budget)
+
+@bp.route('/list/project/<id>', methods=['GET', 'POST'])
+@login_required
+def list_project(id):
+    db = get_db()
+    project = db.execute(
+        'SELECT name, start_date, end_date, details FROM project WHERE id = ?',
+        (id,)
+    ).fetchone()
+
+    if not project:
+        abort(404, "Project doesn't exist.") 
+
+    expenses = db.execute(
+        'SELECT date, type, amount, unit, details FROM expense WHERE project_id = ?',
+        (id,)
+    ).fetchall()
+
+    expense_summary = db.execute(
+        'SELECT IFNULL(exp.type, b.type) AS type, IFNULL(exp.amount, 0) AS amount, IFNULL(exp.unit, "") AS unit, '
+        'IFNULL(b.amount, 0) AS budget_amount, IFNULL(b.unit, "") AS budget_unit, details FROM'
+        '(SELECT type, IFNULL(SUM(amount), 0) AS amount, IFNULL(unit, "") AS unit '
+        'FROM expense WHERE project_id = ? GROUP BY type, unit) AS exp '
+        'FULL OUTER JOIN '
+        '(SELECT type, amount, unit, details FROM budget WHERE project_id = ?) AS b '
+        'ON exp.type = b.type',
+        (id, id)
+    ).fetchall()
+
+    earnings = db.execute(
+        'SELECT date, type, amount, unit, details FROM income WHERE project_id = ?',
+        (id,)
+    ).fetchall()
+
+    earning_summary = db.execute(
+        'SELECT type, IFNULL(SUM(amount), 0) AS amount, IFNULL(unit, "") AS unit '
+        'FROM income WHERE project_id = ? GROUP BY type, unit',
+        (id, )
+    ).fetchall()
+
+    
+    return render_template('account/list_project.html',
+                           project=project,
+                           expenses=expenses, expense_summary=expense_summary,
+                           earnings=earnings, earning_summary=earning_summary)
+
+@bp.route('/list/projects', methods=['GET'])
+@login_required
+def list_projects():
+    db = get_db()
+    projects = db.execute(
+        'SELECT p.id, p.name, p.details, p.start_date, p.end_date, IFNULL(total_income, 0) - IFNULL(total_expense, 0) AS balance, '
+        'IFNULL(IFNULL(i.unit, e.unit), "") AS unit FROM '
+        '(SELECT id, name, details, start_date, end_date FROM project WHERE user_id=? ) AS p '
+        'LEFT OUTER JOIN '
+        '(SELECT project_id, SUM(amount) AS total_expense, unit FROM expense GROUP BY project_id, unit) AS e '
+        'ON e.project_id = p.id '
+        'LEFT OUTER JOIN '
+        '(SELECT project_id, SUM(amount) AS total_income, unit FROM income GROUP BY project_id, unit) AS i '
+        'ON i.project_id = p.id '
+        'WHERE e.unit = i.unit OR e.unit IS NULL OR i.unit IS NULL',        
+        (g.user['id'], )
+        ).fetchall()
+    return render_template('account/list_projects.html', projects=projects)
+
+@bp.route('/list/investment/<id>', methods=['GET', 'POST'])
+@login_required
+def list_investment(id):
+    db = get_db()
+    investment = db.execute(
+        'SELECT name, type, details, invest_date, amount, unit, status FROM investment '
+        'WHERE id = ?',
+        (id,)
+    ).fetchone()
+
+    investment_track = db.execute(
+        'SELECT date, amount, unit FROM investment_track WHERE investment_id = ?',
+        (id, )
+    ).fetchall()
+
+    if request.method == 'POST':
+        track_date = request.form['track_date']
+        amount = request.form['amount']
+        currency = request.form['currency']
+        db.execute(
+            'INSERT INTO investment_track (investment_id, date, amount, unit) '
+            'VALUES (?, ?, ?, ?)',
+            (id, track_date, amount, currency)
+        )
+        db.commit()
+        return redirect(url_for('account.list_investment', id=id))
+
+    return render_template('account/list_investment.html', investment=investment, tracks=investment_track)
 
 
 
--- application/db.py
+++ application/db.py
@@ -36,46 +36,68 @@ def init_app(app):
 
 if __name__ == '__main__':
     db = sqlite3.connect(
-        'test.sqlite',
+        'C:\\Mine\\github\\manage-your-wealth\\instance\\app.sqlite',
         detect_types=sqlite3.PARSE_DECLTYPES
     )
     db.row_factory = sqlite3.Row
-    with open('schema.sql') as f:
-        db.executescript(f.read())
-
-    db.execute(
-        'INSERT INTO user (name, password, email)'
-        ' VALUES (?, ?, ?)',
-        ('MYF', '123', 'myf.py@163.com')
-    )
-
-    db.execute(
-        'INSERT INTO project (user_id, name, start_date, end_date, details)'
-        ' VALUES (?, ?, ?, ?, ?)',
-        (1, '2024-01', datetime.datetime(2024,1,1), datetime.datetime(2024,1,31), '2024-January-life')
-    )
-
-    db.execute(
-        'INSERT INTO expense (user_id, project_id, start_date, end_date, type, amount, details)'
-        ' VALUES (?, ?, ?, ?, ?, ?, ?)',
-        (1, 1, None, None, 'food', 34, 'cake')
-    )
-
-    db.commit()
+    # res = db.execute(
+    #     'SELECT p.id, p.name, p.details, amount, unit FROM expense LEFT OUTER JOIN '
+    #     '(SELECT id, name, details FROM project WHERE user_id=2 AND end_date>="2024-01-03") AS p '
+    #     'ON expense.project_id = p.id'
+    # ).fetchall()
     res = db.execute(
-        'SELECT * FROM user'
-    ).fetchone()
-    for k in res.keys():
-        print(f'{k} = {res[k]}')
+        'SELECT p.id, p.name, p.details, IFNULL(total_income, 0) - IFNULL(total_expense, 0) AS balance, '
+        'IFNULL(IFNULL(i.unit, e.unit), "") AS unit FROM '
+        '(SELECT id, name, details FROM project WHERE user_id=2 AND end_date>="2024-01-03") AS p '
+        'LEFT OUTER JOIN '
+        '(SELECT project_id, SUM(amount) AS total_expense, unit FROM expense GROUP BY project_id, unit) AS e '
+        'ON e.project_id = p.id '
+        'LEFT OUTER JOIN '
+        '(SELECT project_id, SUM(amount) AS total_income, unit FROM income GROUP BY project_id, unit) AS i '
+        'ON i.project_id = p.id '
+        'WHERE e.unit = i.unit OR e.unit IS NULL OR i.unit IS NULL'
+    ).fetchall()
+    for row in res:
+        for k in row.keys():
+            print(f'{k} = {row[k]}', end=',')
+        print()
 
+    res = db.execute('SELECT project_id, SUM(amount) AS total, unit FROM income GROUP BY project_id, unit').fetchall()
+    for row in res:
+        for k in row.keys():
+            print(f'{k} = {row[k]}', end=',')
+        print()
 
-    res = db.execute('SELECT * FROM project').fetchone()
-    for k in res.keys():
-        print(f'{k} = {res[k]}')
+    print()
+    res = db.execute(
+        'SELECT type, IFNULL(SUM(amount), 0) AS amount, IFNULL(unit, "") AS unit '
+        'FROM expense WHERE project_id = 5 GROUP BY type, unit').fetchall()
+    for row in res:
+        for k in row.keys():
+            print(f'{k} = {row[k]}', end=',')
+        print()
 
-    res = db.execute('SELECT * FROM expense').fetchone()
-    for k in res.keys():
-        print(f'{k} = {res[k]}')
+    print()
+    res = db.execute(
+        'SELECT * '
+        'FROM project').fetchall()
+    for row in res:
+        for k in row.keys():
+            print(f'{k} = {row[k]}', end=',')
+        print()
 
-    db.close()
+    print()
+    res = db.execute(
+        'SELECT id, name, type, invest_date, investment.amount, investment.unit, details, '
+        'IFNULL(it.amount, "") AS latest_amount, '
+        'IFNULL(it.unit, "") AS latest_unit FROM investment LEFT OUTER JOIN '
+        '(SELECT investment_id, amount, unit, date FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY investment_id ORDER BY date DESC) AS rn '
+        'FROM investment_track) AS temp WHERE rn = 1) AS it '
+        'ON investment.id = it.investment_id WHERE user_id = 1 AND status = "ongoing" ').fetchall()
+    for row in res:
+        for k in row.keys():
+            print(f'{k} = {row[k]}', end=',')
+        print()
+    
+
 
